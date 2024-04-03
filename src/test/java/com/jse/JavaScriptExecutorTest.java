3dba5ba89fa9be174bdeaf0fb7558e8059becd5d
Rayhan Rahman <rayhanhussain12@gmail.com>
2024-04-03 02:00:35 -0600

    Configured Project

---
+++ src/test/java/com/jse/JavaScriptExecutorTest.java
@@ -0,0 +1,23 @@
+package com.jse;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JavaScriptExecutorTest {
+
+	@Test
+	public void scrollUpDown() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.dell.com/en-us");
+		driver.manage().window().maximize();
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		WebElement playVideoBtn = driver.findElement(By.xpath("(//button[@class='ghpg-play-video-button svg-container dds__button dds__button--primary dds__button--lg'])[1]"));
+		js.executeScript("arguments[0].scrollIntoView(true)", playVideoBtn);	
+	}
+}
