f8310249303b5add683b69dc471ff9f145efdf81
Rayhan Rahman <rayhanhussain12@gmail.com>
2024-04-07 02:13:42 -0600

    new update

--- src/test/java/com/payment/HoverOver.java
+++ src/test/java/com/payment/HoverOver.java
@@ -3,7 +3,7 @@ package com.payment;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,8 +12,8 @@ public class HoverOver {
 
 	@Test
 	public void scrollUpDown() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.dell.com/en-us");
 		driver.manage().window().maximize();
 		WebElement signInElement = driver.findElement(By.id("um-si-label"));
